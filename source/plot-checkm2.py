be3806065337b337e8006063bb6caec6770a3a88
Luis Pedro Coelho <luis@luispedro.org>
2024-05-30 10:27:04 +1000

    ENH Plot results of checkM2 checking

---
+++ source/plot-checkm2.py
@@ -0,0 +1,39 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import polars as pl
+from matplotlib import cm
+colors = cm.Dark2.colors
+import jug
+
+_, jugspace = jug.init('simulate.py', 'simulate.jugdata')
+
+
+q = pl.read_csv(
+        './outputs/simulations/checkm2/quality_report.tsv',
+        separator='\t')
+q = q.with_columns([
+    q['Name'] \
+            .map_elements(lambda s:int(s.split('.')[0].split('_')[-1]), return_dtype=pl.Int32) \
+            .rename("nr_mut")
+    ])
+q = q.sort('nr_mut')
+
+avg_gene_size = np.array([
+    gs.mean() for gs in jug.value(jugspace['gene_sizes'])])
+
+nr_muts = jugspace['nr_muts']
+
+fig,ax1 = plt.subplots()
+ax1.plot(q['nr_mut'], q['Completeness'],'o-', ms=1, c=colors[0], label='Completeness (checkM2)')
+ax1.set_xlabel('Nr mutations')
+ax1.set_ylabel('Completeness')
+
+ax2 = ax1.twinx()
+ax2.plot(nr_muts, avg_gene_size, c=colors[1], label='Avg. gene size')
+ax2.set_ylabel('Avg. gene size')
+fig.legend()
+
+fig.savefig('check2_mutations_50k.png')
+
+ax1.set_xlim(0, 5_000)
+fig.savefig('check2_mutations_5k.png')
