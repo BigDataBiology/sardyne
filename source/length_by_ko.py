3a6a134b3412fddd5c95021a3cf253156e34729b
Luis Pedro Coelho <luis@luispedro.org>
2025-02-11 09:29:12 +1000

    ENH Compute score for extra short genes
    This is likely not going to be the final version of this idea, but it
    does seem to separate WT from mutated versions.

--- source/length_by_ko.py
+++ source/length_by_ko.py
@@ -5,7 +5,6 @@ import numpy as np
 from collections import defaultdict
 import seaborn as sns
 from matplotlib import pyplot as plt
-from fasta import fasta_iter
 import re
 from glob import glob
 import jug
@@ -68,6 +67,12 @@ def load_diamond_outputs(diamond_outs):
         diamond_out['query'].str.split('Ω').list[1].alias('orf'),
         diamond_out['subject'].str.split('~').list[1].alias('KO')
         ])
+    diamond_out = diamond_out.with_columns([
+        diamond_out['genome'].map_elements(
+                                lambda g: int(g.split('_')[1].split('.')[0]),
+                                return_dtype=pl.Int32
+                                ).alias('nr_mutations')
+        ])
     return diamond_out
 
 
@@ -91,19 +96,26 @@ aa_sizes_by_ko = {k:np.array(v) for k,v in aa_sizes_by_ko.items()}
 for v in aa_sizes_by_ko.values():
     v.sort()
 
-nr_seqs_by_ko = dict(gmgc.group_by('KO').len().iter_rows())
+ko_sizes = pl.DataFrame(
+        [ # Multiply by 3 to get nucleotide length
+            [k, v.mean() * 3, (v*3).std()]
+            for k,v in aa_sizes_by_ko.items()],
+        orient='row',
+        schema={
+            'KO': pl.String,
+            'mean': pl.Float32,
+            'std': pl.Float32,
+            }
+        )
 
+view_muts = [0, 100, 1000]
+zscores_outs = []
+esgs_fig,esgs_ax = plt.subplots()
 for tag,_ in jugspace['INPUT_DATA']:
-    view_muts = [0, 100, 1000]
     diamond_out = load_diamond_outputs(glob(f'outputs/checkm2_{tag}_simulation/diamond_output/DIAMOND_RESULTS*.tsv'))
-    diamond_out = diamond_out.with_columns([
-        diamond_out['genome'].map_elements(
-                                lambda g: int(g.split('_')[1].split('.')[0]),
-                                return_dtype=pl.Int32
-                                ).alias('nr_mutations')
-        ])
+    all_muts = sorted(set(diamond_out['nr_mutations']))
     data = []
-    for mut in view_muts:
+    for mut in all_muts:
         diamond_out_mut = diamond_out.filter(pl.col('nr_mutations') == mut)
         f = f'outputs/checkm2_{tag}_simulation/protein_files/mutated_{mut:06}.fna.faa'
         orfs = get_gene_positions(f)
@@ -112,25 +124,26 @@ for tag,_ in jugspace['INPUT_DATA']:
     data = pl.concat(data)
     data = data.filter(pl.col('KO').is_in(aa_sizes_by_ko.keys()))
 
-    zscores = []
-    for ix in range(len(data)):
-        nr_muts, ko, s = data.row(ix)
-        db_size = np.array(aa_sizes_by_ko[ko])*3
+    zscores = data.join(ko_sizes, left_on='KO', right_on='KO').with_columns(
+        z=(pl.col('length')- pl.col('mean'))/pl.col('std')
+        )[['nr_mutations', 'KO', 'z']]
 
-        zscores.append((nr_muts,
-                        ko,
-                        (s - db_size.mean())/db_size.std(),
-                        ))
+    esgs = []
+    for mut in all_muts:
+        sel = zscores.filter((pl.col('nr_mutations') == mut) & (pl.col('z') < -4)).select(pl.col('z').sum())
+        esgs.append(sel.item())
 
-    zscores = pl.DataFrame(zscores, schema={
-        'nr_muts': pl.Int32,
-        'KO': pl.String,
-        'z': pl.Float32,
-        },
-        orient='row')
+    [ell] = esgs_ax.plot(all_muts, esgs, label=tag)
+    esgs_ax.plot(all_muts, [esgs[0] for _ in all_muts], label=None, linestyle='--', color=ell.get_color())
+    esgs_ax.set_xlabel('Number of mutations')
+    esgs_ax.set_ylabel('Sum of z-scores < -4')
+
+    view_muts = [0, 100, 1000]
+    zscores = zscores.filter(pl.col('nr_mutations').is_in(view_muts))
+
     fig, ax = plt.subplots()
     sns.boxplot(data=zscores,
-                x='nr_muts',
+                x='nr_mutations',
                 y='z',
                 boxprops=dict(alpha=.4, facecolor='w'),
                 width=.2,
@@ -142,7 +155,7 @@ for tag,_ in jugspace['INPUT_DATA']:
     fig,ax = plt.subplots()
     X = np.linspace(-12,12,1000)
     for lim,lab in [(0, 'WT'), (100, '100'), (1000, '1k')]:
-        sel = zscores.filter(pl.col('nr_muts') == lim)
+        sel = zscores.filter(pl.col('nr_mutations') == lim)
         ax.plot(X, [(sel['z'] < x).mean() for x in X], label=lab)
     ax.legend()
     ax.set_xlabel('KO length z-score')
@@ -154,10 +167,15 @@ for tag,_ in jugspace['INPUT_DATA']:
                 zscores.with_columns([
                         (zscores['z'] < lim).alias('bel_lim')
                         ]) \
-                    .group_by('nr_muts') \
-                    .sum()[['nr_muts', 'bel_lim']] \
+                    .group_by('nr_mutations') \
+                    .sum()[['nr_mutations', 'bel_lim']] \
                     .with_columns([pl.lit(lim).alias('lim')])
                 for lim in [-2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12]])
-    zscores_thresh = zscores_thresh.pivot(on=['nr_muts'], values=['bel_lim'], index=['lim'])
-    print(tag)
-    print(zscores_thresh['lim', '0', '100', '1000'])
+    zscores_thresh = zscores_thresh.pivot(on=['nr_mutations'], values=['bel_lim'], index=['lim'])
+    zscores_outs.append(zscores_thresh.with_columns(tag=pl.lit(tag)))
+
+esgs_ax.legend()
+sns.despine(esgs_fig, trim=True)
+esgs_fig.tight_layout()
+esgs_fig.savefig('plots/esgs_m4.svg')
+esgs_fig.savefig('plots/esgs_m4.png')
