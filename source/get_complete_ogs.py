1cc6f8cf3f2c566c6c44251d19dcd2c85b0e91e5
Luis Pedro Coelho <luis@luispedro.org>
2025-04-29 09:40:46 +1000

    BUG Fix eggnog-mapper GMGCv1 background parsing

--- source/get_complete_ogs.py
+++ source/get_complete_ogs.py
@@ -20,23 +20,33 @@ def complete_og_size_table():
     """
     Get the complete ogs from the GMGC10 dataset.
     """
+    import subprocess
+    from os import path, unlink
+
     complete_sizes = []
     for h,seq in fasta_iter('../data/data/GMGC10.95nr.complete.faa.gz'):
         complete_sizes.append([h, len(seq)])
 
     complete_sizes = pl.DataFrame(complete_sizes, schema=['#query_name', 'aa_size'], orient='row')
+    print("Complete sizes loaded")
 
     annotations = pl.scan_csv('../data/data/GMGC10.emapper2.annotations.tsv',
                     comment_prefix='# ',
                     separator='\t')
-    annotations = annotations.select(['#query_name', 'eggNOG_OGs']).collect()
     annotations = annotations.with_columns(
-            eggNOG_OG1=annotations['eggNOG_OGs'].map_elements(extract_og, return_dtype=str))
-    annotations = annotations[['#query_name', 'eggNOG_OG1']]
+            eggNOG_OG1=pl.col('eggNOG_OGs').map_elements(extract_og, return_dtype=str))
+    annotations = annotations.select(['#query_name', 'eggNOG_OG1'])
+    print("Annotations loaded (lazy)")
 
-    annotations = annotations.join(complete_sizes, on='#query_name', how='inner')
+    annotations = annotations.join(complete_sizes.lazy(), on='#query_name', how='inner')
+    print("Annotations joined with complete sizes")
+
     oname = 'outputs/GMGC10.emapper2.annotations.complete.tsv'
-    annotations.write_csv(oname,
-                          separator='\t',
-                          )
-    return oname
+    annotations.sink_csv(oname,
+                           separator='\t',
+                           )
+    if path.exists(oname + '.gz'):
+        unlink(oname + '.gz')
+    subprocess.check_call(['gzip', oname])
+    return oname + '.gz'
+
